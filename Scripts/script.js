5269f2d6cb04915edeb0920e642f02889de4939f
meliberto7 <130302288+meliberto7@users.noreply.github.com>
2024-08-11 21:57:47 -0300

    Primeiro commit

---
+++ Scripts/script.js
@@ -0,0 +1,23 @@
+function abreGit() {
+
+    var url = "https://github.com/meliberto7?tab=repositories";
+    
+    window.open(url);
+
+}
+
+function abreInsta() {
+
+    var url = "https://www.instagram.com/r_henr7que__/?next=%2F";
+    
+    window.open(url);
+
+}
+
+function abreGmail() {
+
+    var url = "https://mail.google.com/mail/u/0/#inbox";
+    
+    window.open(url);
+
+}
